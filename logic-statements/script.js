ec737f4279e9085b7074a96d490f910f875b745d
mwallert <michael.wallert@gmail.com>
2019-07-28 15:30:55 -0700

    feat(logic-statements): Adds logic statements for apples

--- logic-statements/script.js
+++ logic-statements/script.js
@@ -1,11 +1,39 @@
 ;(function(){
-    let hasApples = 1;
+    // Buttons
+    const addButton = document.querySelector('#add-button'),
+        removeButton = document.querySelector('#remove-button');
 
-    if (hasApples) {
-        console.log(`We has ${hasApples} apples!`);
-    } else if (!hasApples) {
-        console.log('We have no apples :(');
-    } else {
-        console.log('This is a fail safe else statement.');
+    let appleCount = 0;
+
+    const addApples = () => {
+        appleCount++;
+        doWeHaveApples();
     }
+
+    const removeApples = () => {
+        if (appleCount > 0) {
+            appleCount--;
+            doWeHaveApples();
+        } else {
+            console.log('You cannot remove apples you dont have!')
+        }
+    }
+
+    const doWeHaveApples = () => {
+        if (appleCount > 0) {
+            console.log(`We has ${appleCount} apples!`);
+        } else if (appleCount === 0) {
+            console.log('We have no apples :(');
+        } else {
+            console.log(`You now have a negative apple count of: ${appleCount}`);
+        }
+    }
+
+    addButton.addEventListener('click', (e) => {
+        addApples();
+    });
+
+    removeButton.addEventListener('click', (e) => {
+        removeApples();
+    });
 }());
